370b01a65d9cc1b66cd0695e61d3b96e3811d7de
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-24 08:01:01 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include "main.h"
 
+//function definition to get the data range
 void getRange(int* data, int dataCnt, char* buff)
 {
 
 }
 
+//main function
 int main(void)
 {
     int rangeData[]={4,5};
     char rangeOutput[20];
-
     getRange(rangeData, 2, rangeOutput);
     assert(strcmp(rangeOutput,"Range, Readings\n4-5, 2\n") == 0);
-
     return 0;
 }
