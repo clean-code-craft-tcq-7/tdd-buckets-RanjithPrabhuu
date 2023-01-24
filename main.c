f85b3f7643666327603a5c3d1b1cfcf9f989cf19
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-24 07:58:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
-#include "ccp_finder.h"
-
-int main()
+void getRange(int* data, int dataCnt, char* buff)
 {
-    int current_pair[2] = {4,5};
-    char ccp_pair_output[100];
-    memset(ccp_pair_output, 0, 100);
-    findCurrentPairs(current_pair, 2, ccp_pair_output);
-    assert(strcmp(ccp_pair_output, "Range, Readings\n4-5, 2\n"));
+
+}
+
+int main(void)
+{
+    int rangeData[]={4,5};
+    char rangeOutput[20];
+
+    getRange(rangeData, 2, rangeOutput);
+    assert(strcmp(rangeOutput,"Range, Readings\n4-5, 2\n") == 0);
+
     return 0;
 }
