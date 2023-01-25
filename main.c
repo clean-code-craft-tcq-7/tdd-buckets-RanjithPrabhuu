4916bfee1c60f91f50c4387e48339865f7d866ee
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-25 08:10:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,53 @@
 //function definition to get the data range
 void getRange(int* data, int dataCnt, char* buff)
 {
+    char printData[100];
+    int cntInRange;
 
+    memset(printData, 0, 100);
+    sortInAscending(data,dataCnt);
+    cntInRange = getCountInRange(data, dataCnt, data[0], data[dataCnt - 1]);
+    sprintf(printData,"%s\n%d-%d, %d\n",HEADER_CHAR,data[0],data[dataCnt - 1], cntInRange);
+    strncpy(buff,printData,strlen(printData));
+}
+
+//function definition to find the count which is in range
+int getCountInRange(int* data, int cnt, int min, int max)
+{
+    int i,occurCnt = 0;
+    for(i=0; i< cnt; i++)
+    {
+        occurCnt += checkWithinRange(data[i], min, max);
+    }
+    return occurCnt;
+}
+
+//function definition to the range
+int checkWithinRange(int value, int min, int max)
+{
+    if(value >= min || value <= max)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+//function definition to sort the data in ascending
+void sortInAscending(int* data, int length)
+{
+    int temp,i,j;
+    for(i = 0; i < length; i++)
+    {
+        for(j = i + 1; j < length; j++)
+        {
+            if(data[i] > data[j])
+            {
+                temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+    }
 }
 
 //main function
