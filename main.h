0f8783a65be5547c99eb4236a261beb55b5a043c
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-25 08:11:38 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,2 +1,4 @@
 //function declaration to get the range
 void getRange(int* data, int dataCnt, char* buff);
+//function definition to find the count which is in range
+int getCountInRange(int* data, int cnt, int min, int max)
