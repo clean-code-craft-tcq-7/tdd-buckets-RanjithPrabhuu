0371052ff86e23ab9b65d2418bc3f6401549c325
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-25 08:15:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -58,9 +58,18 @@ void sortInAscending(int* data, int length)
 //main function
 int main(void)
 {
-    int rangeData[]={4,5};
-    char rangeOutput[20];
-    getRange(rangeData, 2, rangeOutput);
-    assert(strcmp(rangeOutput,"Range, Readings\n4-5, 2\n") == 0);
+    char output[100];
+    memset(output, 0, 100);
+
+    // Test case to check the count
+    int data[]={4,5};
+    getRangeData(data, 2, output);
+    assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
+    // Test case to check in reverse order
+    int data1[] = {5,4};
+    memset(output, 0, 100);
+    getRangeData(data1, 2, output);
+    assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
+    
     return 0;
 }
