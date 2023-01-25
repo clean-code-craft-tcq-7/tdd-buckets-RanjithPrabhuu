628dbbf632adf05188e3273ab4d201dc0ae2c058
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-25 08:17:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -63,12 +63,13 @@ int main(void)
 
     // Test case to check the count
     int data[]={4,5};
-    getRangeData(data, 2, output);
+    getRange(data, 2, output);
     assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
+    
     // Test case to check in reverse order
     int data1[] = {5,4};
     memset(output, 0, 100);
-    getRangeData(data1, 2, output);
+    getRange(data1, 2, output);
     assert(strcmp(output,"Range, Readings\n4-5, 2\n") == 0);
     
     return 0;
