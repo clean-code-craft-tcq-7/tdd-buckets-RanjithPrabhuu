83ef96ab68a0c4dff4ea263218902bd8bee8cef6
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-02-01 05:58:52 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
 //function declaration to get the range
 void getRange(int* data, int dataCnt, char* buff);
-//function declaration to find the count which is in range
-int getCountInRange(int* data, int cnt, int min, int max);
-//function declaration to the range
-int checkRange(int value, int min, int max);
+//function definition to get the data range and count
+void getRangesAndCount(int* data, int len,char* strVal);
+//function definition to check whether the data is consecutive
+int isConsecutive(int* arr, int min, int len);
 //function declaration to sort the data in ascending
 void orderAscending(int* data, int length);
